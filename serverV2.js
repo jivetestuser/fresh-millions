0250790f91f87483e0b6570fe5acb7067d87bd7f
PGovindaraj <s0171925@Enigma.local>
2015-07-30 15:02:36 -0700

    made changes to email format string.

--- serverV2.js
+++ serverV2.js
@@ -37,13 +37,26 @@ Resource.after('post', function(req, res, next){
 });
 
 app.post('/orders', function (req, res, next) {
-	var emailString = ('item:' + req.body.itemName + ' -- ');
-	emailString += ('quantity:' + req.body.quantity + ' -- ');
-	emailString += ('price:' + req.body.price + ' --');
-	emailString += ('subTotal' + req.body.subTotal + ' -- ');
-	emailString += ('tax' + req.body.tax + ' -- ');
-	emailString += ('tip' + req.body.tip + ' --');
-	emailString += ('total' + req.body.total);
+	var emailString = ('customer name:' + req.body.customerName + '\n');
+	emailString += ('customer phone:' + req.body.customerPhone + '\n');
+	emailString += ('restaurant address:' + req.body.restaurantAddress + '\n');
+
+	var now = new Date();
+	var strDateTime = [[AddZero(now.getDate()), AddZero(now.getMonth() + 1), now.getFullYear()].join("/"), [AddZero(now.getHours()), AddZero(now.getMinutes())].join(":"), now.getHours() >= 12 ? "PM" : "AM"].join(" ");
+
+	//Pad given value to the left with "0"
+	function AddZero(num) {
+    	return (num >= 0 && num < 10) ? "0" + num : num + "";
+	}
+	
+	emailString += ('order date:' + now + '\n');
+	emailString += ('item:' + req.body.itemName + '\n');
+	emailString += ('quantity:' + req.body.quantity + '\n');
+	emailString += ('price:' + req.body.price + '\n');
+	emailString += ('subTotal:' + req.body.subTotal + '\n');
+	emailString += ('tax:' + req.body.tax + '\n');
+	emailString += ('tip:' + req.body.tip + '\n');
+	emailString += ('total:' + req.body.total);
 
 	console.log(emailString);
 
